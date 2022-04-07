c55c0bd67747d6874037d68a98753a391445bcb6
Yoan BENSIMHON <ybensimhon@manzalab.com>
2022-04-07 09:35:52 +0200

    add banner

--- wordpress/wp-content/plugins/marmishlag12_plugin/marmishlag12_plugin_banner.php
+++ wordpress/wp-content/plugins/marmishlag12_plugin/marmishlag12_plugin_banner.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Marmishlag Plugin Banner
- * Description: Ce plugin permettra d'afficher la banierre
+ * Description: Ce plugin permettra d'afficher la banniere
  * Version: 1.0.0
  * Author: Intisar OULBANI
  * Author URI: https://www.linkedin.com/in/intisar-el-hadj-etahar-b07759194/
@@ -9,6 +9,6 @@
 
 ?>
 
-<div class="relative text-center py-3 px-5 bg-amber-400 text-black text-sm rounded-md">
-En panne d’inspiration! Découvrez nos différentes et savoureuses <strong>recettes</strong> !
+<div class="text-center py-3 px-5 text-yellow-900 text-sm rounded-md border border-yellow-200" style="background-color: #ece8bd;">
+En panne d’inspiration! Découvrez nos différentes et savoureuses <strong>recettes</strong>!
 </div>
